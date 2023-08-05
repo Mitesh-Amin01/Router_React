1193d9a9040442056dea9815d3d5eeabfe5ed9cf
Mitesh-Amin01 <aminmiteshkumar.1484@gmail.com>
2023-08-05 10:31:18 +0530

    Initialize Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './style.css'
 
 export default function navbar() {
   return (
-    <div>
+    <div id='div'>
         <Link to="/">Home</Link>
         <Link to="/contect">Contect</Link>
         <Link to="/about">About</Link>
